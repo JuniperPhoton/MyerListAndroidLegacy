ecf763e07eea13e38c7378a7a419f595208f8188
juniperphoton <dengweichao@hotmail.com>
2015-05-29 14:32:06 +0800

    inital

--- app/src/main/java/com/example/juniper/myerlistandroid/MainActivity.java
+++ app/src/main/java/com/example/juniper/myerlistandroid/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.juniper.myerlistandroid;
 
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +13,14 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+
+
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        }
+
         setContentView(R.layout.activity_main);
     }
 

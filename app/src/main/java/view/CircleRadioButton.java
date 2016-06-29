9d4b58986ac570f460758433d9c260bf16b81197
juniperphoton <dengweichao@hotmail.com>
2016-06-29 11:38:23 +0800

    Start make CircleRadioButton.

--- app/src/main/java/view/CircleRadioButton.java
+++ app/src/main/java/view/CircleRadioButton.java
@@ -1,7 +1,26 @@
 package view;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+import com.juniperphoton.myerlistandroid.R;
+
+import java.util.jar.Attributes;
+
 /**
  * Created by dengw on 2016-06-29.
  */
-public class CircleRadioButton {
+public class CircleRadioButton extends RadioButton {
+    public CircleRadioButton(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+
+
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawBitmap(getResources().getDrawable(R.drawable.ok));
+    }
 }

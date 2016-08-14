e56f1a937e8b5a66001bbbb573183e3bb61333c5
juniperphoton <dengweichao@hotmail.com>
2016-08-15 01:19:48 +0800

    Remove drawer selected delay.

--- app/src/main/java/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/fragment/NavigationDrawerFragment.java
@@ -266,7 +266,7 @@ public class NavigationDrawerFragment extends Fragment implements INavigationDra
                 public void run() {
                     mDrawerLayout.closeDrawer(mFragmentContainerView);
                 }
-            }, 300);
+            }, 10);
         }
     }
 
